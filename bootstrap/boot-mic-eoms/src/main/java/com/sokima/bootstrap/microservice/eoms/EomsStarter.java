178de82ba5454994048a2d5ed970386378fdcae6
sokimaaa <romandenysov9@gmail.com>
2023-12-03 22:44:25 +0200

    [TASK-09] add bootstrap circle

---
+++ bootstrap/boot-mic-eoms/src/main/java/com/sokima/bootstrap/microservice/eoms/EomsStarter.java
@@ -0,0 +1,13 @@
+package com.sokima.bootstrap.microservice.eoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EomsStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EomsStarter.class, args);
+    }
+
+}
---
+++ bootstrap/boot-mic-epcs/src/main/java/com/sokima/bootstrap/microservice/epcs/EpcsStarter.java
@@ -0,0 +1,13 @@
+package com.sokima.bootstrap.microservice.epcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpcsStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpcsStarter.class, args);
+    }
+
+}
