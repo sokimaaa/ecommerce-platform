1ac1d3771fa7b565c3f4596ed4c02b7d61280e26
sokimaaa <romandenysov9@gmail.com>
2023-12-04 22:27:24 +0200

    [TASK-11] add mock eums implementation

--- infrastructure/infra-mic-eums/src/main/java/com/sokima/infrastructure/microservice/eums/adapter/driving/UserReactiveRestAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.sokima.infrastructure.microservice.eums.adapter.driving;
-
-import com.sokima.specification.rest.user.auth.v1.api.UserAuthenticationReactiveApi;
-import com.sokima.specification.rest.user.auth.v1.api.UserRegistrationReactiveApi;
-import com.sokima.specification.rest.user.modify.v1.api.UserModificationReactiveApi;
-
-public class UserReactiveRestAdapter implements UserAuthenticationReactiveApi, UserModificationReactiveApi, UserRegistrationReactiveApi {
-}
