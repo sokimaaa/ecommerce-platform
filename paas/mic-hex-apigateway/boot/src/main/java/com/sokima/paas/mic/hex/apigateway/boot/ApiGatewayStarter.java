b224d57d79671bdf53c9324902bfcb47638c678e
rdenysov <roman.denysov@intellias.com>
2024-01-01 20:11:23 +0200

    [TASK-15] remove redundant imports

--- paas/mic-hex-apigateway/boot/src/main/java/com/sokima/paas/mic/hex/apigateway/boot/ApiGatewayStarter.java
+++ paas/mic-hex-apigateway/boot/src/main/java/com/sokima/paas/mic/hex/apigateway/boot/ApiGatewayStarter.java
@@ -2,8 +2,6 @@ package com.sokima.paas.mic.hex.apigateway.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayStarter {
