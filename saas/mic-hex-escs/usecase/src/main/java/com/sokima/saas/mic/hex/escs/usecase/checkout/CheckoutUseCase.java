99f97b50a9e5f4e04e32aacb1c4247322759dce2
rdenysov <roman.denysov@intellias.com>
2024-01-05 13:58:08 +0200

    [TASK-17] implement checkout usecase

---
+++ saas/mic-hex-escs/usecase/src/main/java/com/sokima/saas/mic/hex/escs/usecase/checkout/CheckoutUseCase.java
@@ -0,0 +1,43 @@
+package com.sokima.saas.mic.hex.escs.usecase.checkout;
+
+import com.sokima.lib.proto.spec.eoms.CompletionOrderCreation;
+import com.sokima.saas.mic.hex.escs.domain.mapper.CheckoutInitiationMapper;
+import com.sokima.saas.mic.hex.escs.domain.persistent.port.outbound.cart.FindCartPersistentOutPort;
+import com.sokima.saas.mic.hex.escs.domain.service.CheckoutInitiationService;
+import com.sokima.saas.mic.hex.escs.domain.validator.CartValidatorChain;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+
+public final class CheckoutUseCase {
+
+    private static final Logger log = LoggerFactory.getLogger(CheckoutUseCase.class);
+
+    private final FindCartPersistentOutPort findCartPersistent;
+
+    private final CheckoutInitiationService checkoutInitiationService;
+
+    private final CartValidatorChain cartRuleValidatorChain;
+
+    public CheckoutUseCase(
+            final FindCartPersistentOutPort findCartPersistent,
+            final CheckoutInitiationService checkoutInitiationService,
+            final CartValidatorChain cartRuleValidatorChain
+    ) {
+        this.findCartPersistent = findCartPersistent;
+        this.checkoutInitiationService = checkoutInitiationService;
+        this.cartRuleValidatorChain = cartRuleValidatorChain;
+    }
+
+    public Mono<CompletionOrderCreation> checkout(final Long cartId, final String shippingAddress, final String paymentMethod) {
+        return findCartPersistent.findSingleCart(cartId)
+                .doOnNext(cart -> {
+                    log.debug("Found Cart[value={}] to being checkout.", cart);
+                    final var isValid = cartRuleValidatorChain.validateCart(cart);
+                    // rollback transaction
+                })
+                .map(cart -> CheckoutInitiationMapper.composeInitiateOrderCreation(cart.userId(), cart.productIds(), shippingAddress, paymentMethod))
+                .flatMap(checkoutInitiationService::initiateOrderCreation)
+                .doOnNext(x -> log.info("Order for Cart[cartId={}] was initiated.", cartId));
+    }
+}
