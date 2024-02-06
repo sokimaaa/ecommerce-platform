f6597ec2c0d54b275ed9cb84b8e472c410ba3b4b
sokimaaa <romandenysov9@gmail.com>
2024-02-06 23:45:08 +0200

    Revert "[TASK-17] revert eoms"
    This reverts commit c619779049b0bffea73ee158aa5af92c2d207d75.
    
    	deleted:    saas/mic-hex-eoms/domain/src/main/java/com/sokima/saas/mic/hex/eoms/domain/checksum/ChecksumGenerator.java
    	deleted:    saas/mic-hex-eoms/domain/src/main/java/com/sokima/saas/mic/hex/eoms/domain/checksum/InitiateOrderCreationChecksumGenerator.java
    	deleted:    saas/mic-hex-eoms/domain/src/main/java/com/sokima/saas/mic/hex/eoms/domain/mapper/GetProductMapper.java
    	deleted:    saas/mic-hex-eoms/domain/src/main/java/com/sokima/saas/mic/hex/eoms/domain/service/duplication/OrderInitiationRequestDuplicationService.java
    	deleted:    saas/mic-hex-eoms/domain/src/main/java/com/sokima/saas/mic/hex/eoms/domain/service/duplication/RequestDuplicationService.java
    	deleted:    saas/mic-hex-eoms/domain/src/main/java/com/sokima/saas/mic/hex/eoms/domain/service/product/GetProductService.java
    	deleted:    saas/mic-hex-eoms/domain/src/main/java/com/sokima/saas/mic/hex/eoms/domain/service/product/HoldProductService.java

--- saas/mic-hex-eoms/domain/src/main/java/com/sokima/saas/mic/hex/eoms/domain/mapper/GetProductMapper.java
+++
@@ -1,7 +0,0 @@
-package com.sokima.saas.mic.hex.eoms.domain.mapper;
-
-public interface GetProductMapper {
-
-
-
-}
--- saas/mic-hex-eoms/domain/src/main/java/com/sokima/saas/mic/hex/eoms/domain/service/duplication/RequestDuplicationService.java
+++
@@ -1,9 +0,0 @@
-package com.sokima.saas.mic.hex.eoms.domain.service.duplication;
-
-import reactor.core.publisher.Mono;
-
-public interface RequestDuplicationService<R> {
-
-    Mono<Boolean> isDuplicated(final R request);
-
-}
