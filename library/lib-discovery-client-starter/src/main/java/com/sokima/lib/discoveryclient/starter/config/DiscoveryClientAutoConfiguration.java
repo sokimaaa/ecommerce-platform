ddd06301ce33cc8aff24589700f42dcbdffaddc8
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2024-02-17 17:17:09 +0200

    [TASK-24] add profiles configuration to eims service

--- library/lib-discovery-client-starter/src/main/java/com/sokima/lib/discoveryclient/starter/config/DiscoveryClientAutoConfiguration.java
+++ library/lib-discovery-client-starter/src/main/java/com/sokima/lib/discoveryclient/starter/config/DiscoveryClientAutoConfiguration.java
@@ -1,11 +1,14 @@
 package com.sokima.lib.discoveryclient.starter.config;
 
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
+@Configuration
+@Profile("cloud")
 @EnableDiscoveryClient
 @PropertySource(value = "classpath:application-discovery-client.properties")
-@AutoConfiguration
 public class DiscoveryClientAutoConfiguration {
+
 }
