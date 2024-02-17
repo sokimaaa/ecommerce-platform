fd6292b6a8d07ec2c38439d0edcd0b5994bccfc6
sokimaaa <romandenysov9@gmail.com>
2024-02-17 23:09:42 +0200

    [TASK-23] rename vars

--- library/lib-building-block-flow/src/main/java/com/sokima/lib/building/block/flow/BiFlow.java
+++ library/lib-building-block-flow/src/main/java/com/sokima/lib/building/block/flow/BiFlow.java
@@ -5,7 +5,7 @@ import reactor.core.publisher.Flux;
 import reactor.util.function.Tuple2;
 
 public interface BiFlow<P1, P2, R> extends Flow<Tuple2<P1, P2>, R> {
-    default Flux<R> doFlow(final P1 leftPayload, final P2 rightPayload) {
-        return doFlow(MergingTransformer.merge(leftPayload, rightPayload));
+    default Flux<R> doFlow(final P1 rightPayload, final P2 leftPayload) {
+        return doFlow(MergingTransformer.merge(rightPayload, leftPayload));
     }
 }
