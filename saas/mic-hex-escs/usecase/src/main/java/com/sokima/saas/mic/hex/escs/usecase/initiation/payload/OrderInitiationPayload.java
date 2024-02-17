af2f43e89a0649990860ee4d4f84637904afa84a
sokimaaa <romandenysov9@gmail.com>
2024-02-17 23:03:56 +0200

    [TASK-23] adapt usecase layer to domain layer

--- saas/mic-hex-escs/usecase/src/main/java/com/sokima/saas/mic/hex/escs/usecase/initiation/payload/OrderInitiationPayload.java
+++
@@ -1,8 +0,0 @@
-package com.sokima.saas.mic.hex.escs.usecase.initiation.payload;
-
-public record OrderInitiationPayload(
-        Long cartId,
-        String shippingAddress,
-        String paymentMethod
-) {
-}
