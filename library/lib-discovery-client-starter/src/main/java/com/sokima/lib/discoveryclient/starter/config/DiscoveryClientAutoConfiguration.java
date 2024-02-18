e34766420a1e689e6148bd94ccd66e25677a052c
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2024-02-18 12:35:03 +0200

    [TASK-24] replace @Profile to @ConditionalOnProperty

--- library/lib-discovery-client-starter/src/main/java/com/sokima/lib/discoveryclient/starter/config/DiscoveryClientAutoConfiguration.java
+++ library/lib-discovery-client-starter/src/main/java/com/sokima/lib/discoveryclient/starter/config/DiscoveryClientAutoConfiguration.java
@@ -1,12 +1,12 @@
 package com.sokima.lib.discoveryclient.starter.config;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @AutoConfiguration
-@Profile("cloud")
+@ConditionalOnProperty(havingValue = "eureka.client.enabled", value = "true", matchIfMissing = false)
 @EnableDiscoveryClient
 @PropertySource(value = "classpath:application-discovery-client.properties")
 public class DiscoveryClientAutoConfiguration {
