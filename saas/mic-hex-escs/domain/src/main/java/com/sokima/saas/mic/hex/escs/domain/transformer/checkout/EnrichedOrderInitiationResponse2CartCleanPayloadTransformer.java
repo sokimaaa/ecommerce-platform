d5bea299d0e973519859b2682e4845f0f03fe8f6
sokimaaa <romandenysov9@gmail.com>
2024-02-20 23:04:07 +0200

    [TASK-25] implement enrich order init resp transformer

--- saas/mic-hex-escs/domain/src/main/java/com/sokima/saas/mic/hex/escs/domain/transformer/checkout/EnrichedOrderInitiationResponse2CartCleanPayloadTransformer.java
+++ saas/mic-hex-escs/domain/src/main/java/com/sokima/saas/mic/hex/escs/domain/transformer/checkout/EnrichedOrderInitiationResponse2CartCleanPayloadTransformer.java
@@ -3,10 +3,16 @@ package com.sokima.saas.mic.hex.escs.domain.transformer.checkout;
 import com.sokima.lib.building.block.transformer.Transformer;
 import com.sokima.saas.mic.hex.escs.domain.payload.clean.CleanCartPayload;
 import com.sokima.saas.mic.hex.escs.domain.payload.initiation.EnrichedOrderInitiationResponse;
+import com.sokima.saas.mic.hex.escs.domain.transformer.checkout.mapper.EnrichedOrderInitiationResponse2CartCleanPayloadMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class EnrichedOrderInitiationResponse2CartCleanPayloadTransformer implements Transformer<EnrichedOrderInitiationResponse, CleanCartPayload> {
+    private static final Logger log = LoggerFactory.getLogger(EnrichedOrderInitiationResponse2CartCleanPayloadTransformer.class);
+
     @Override
     public CleanCartPayload transform(final EnrichedOrderInitiationResponse input) {
-        throw new UnsupportedOperationException("Not implemented");
+        log.debug("Transforming enriched order init response: {}", input);
+        return EnrichedOrderInitiationResponse2CartCleanPayloadMapper.INSTANCE.map(input);
     }
 }
